53f29f730e70f017350dee2b4510a4685c587384
D054910 <D054910@192.168.1.168>
2015-10-19 22:42:37 +0200

    mbean enhancement: callActor

--- HomeControl/src/main/java/de/jumajumo/homecontrol/monitoring/jmx/TestMBeanImpl.java
+++ HomeControl/src/main/java/de/jumajumo/homecontrol/monitoring/jmx/TestMBeanImpl.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Component;
 
 import de.jumajumo.core.service.weather.WeatherService;
 import de.jumajumo.homecontrol.configuration.ConfigurationContextHolder;
+import de.jumajumo.homecontrol.configuration.client.Device;
 import de.jumajumo.homecontrol.configuration.server.Condition;
 import de.jumajumo.homecontrol.service.ConditionService;
+import de.jumajumo.homecontrol.service.DeviceConfigurationService;
 
 @Component
 @ManagedResource(objectName = "de.jumajumo.homecontrol.monitoring.jmx:name=TestBean", description = "the test bean")
@@ -26,11 +28,25 @@ public class TestMBeanImpl implements TestMBean
 	@Autowired
 	private WeatherService weatherService;
 
+	@Autowired
+	private DeviceConfigurationService deviceConfigurationService;
+
 	@Override
 	@ManagedAttribute
 	public String getValue()
 	{
 		return "the value";
+	}
+
+	@ManagedOperation
+	public void callActor(final String actorUuid)
+	{
+		final UUID uuid = UUID.fromString(actorUuid);
+
+		final Device device = this.deviceConfigurationService
+				.findDeviceByActorUuid(uuid);
+
+		this.deviceConfigurationService.callActor(device, uuid);
 	}
 
 	@ManagedOperation

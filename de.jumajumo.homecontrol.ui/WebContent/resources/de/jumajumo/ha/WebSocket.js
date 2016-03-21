22b0932383dcad16b99fd55093dde2497a95a86e
Jürgen Buck <j.buck@sap.com>
2016-03-21 22:31:06 +0100

    websocket: enable auto refresh
    Change-Id: I558e01f2008d4165b6689e229178288b5451cc48

--- de.jumajumo.homecontrol.ui/WebContent/resources/de/jumajumo/ha/WebSocket.js
+++ de.jumajumo.homecontrol.ui/WebContent/resources/de/jumajumo/ha/WebSocket.js
@@ -2,26 +2,39 @@ jQuery.sap.declare("jumajumo.ha.WebSocket");
 
 jumajumo.ha.WebSocket =
 {
+	currentConnection : null,
 	connection : function()
 	{
-		return connection;
+		var that = this;
+
+		if (null == this.currentConnection)
+		{
+			this.currentConnection = this.createNewConnection();
+
+			this.currentConnection.onopen = function()
+			{
+				var json = '{"command" : "DEFINE_DEVICE","parameters" : {"deviceId" : "9a8bed96-230e-4b37-b030-4e3d1795996f"}}';
+				that.currentConnection.send(json);
+			};
+
+			this.currentConnection.onmessage = function(e)
+			{
+			};
+
+			this.currentConnection.onclose = function()
+			{
+				that.currentConnection = null;
+			};
+		}
+
+		return this.currentConnection;
+	},
+
+	createNewConnection : function()
+	{
+		var conn = new WebSocket('ws://' + window.location.host
+				+ '/HomeControlServer/dispatcher/websocketapi');
+
+		return conn;
 	}
-};
-
-var connection = new WebSocket('ws://' + window.location.host
-		+ '/HomeControlServer/dispatcher/websocketapi');
-
-connection.onopen = function()
-{
-
-	var json = '{"command" : "DEFINE_DEVICE","parameters" : {"deviceId" : "9a8bed96-230e-4b37-b030-4e3d1795996f"}}';
-	connection.send(json);
-};
-
-connection.onmessage = function(e)
-{
-};
-
-connection.onclose = function()
-{
 };

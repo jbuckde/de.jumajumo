d1edafe22b03075b06abbde73bf9426bffebd76f
D054910 <j.buck@sap.com>
2017-12-01 20:32:11 +0100

    new ui project
    Change-Id: I48574afa0f90e69f5bd49aab1ab54678e904ab8e

---
+++ de.jumajumo.homecontrol.ui.new/controller/App.controller.js
@@ -0,0 +1,68 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    'sap/m/MessageToast',
+    'sap/ui/Device'
+], function (Controller, MessageToast, Device) {
+    "use strict";
+
+    return Controller.extend("de.jumajumo.homecontrol.ui.controller.App", {
+        
+        /**
+         * Convenience method for accessing the router.
+         * @public
+         * @returns {sap.ui.core.routing.Router} the router for this component
+         */
+        getRouter : function () {
+            return sap.ui.core.UIComponent.getRouterFor(this);
+        },
+
+        /**
+         * Convenience method for getting the view model by name.
+         * @public
+         * @param {string} [sName] the model name
+         * @returns {sap.ui.model.Model} the model instance
+         */
+        getModel : function (sName) {
+            return this.getView().getModel(sName);
+        },
+
+        /**
+         * Convenience method for setting the view model.
+         * @public
+         * @param {sap.ui.model.Model} oModel the model instance
+         * @param {string} sName the model name
+         * @returns {sap.ui.mvc.View} the view instance
+         */
+        setModel : function (oModel, sName) {
+            return this.getView().setModel(oModel, sName);
+        },
+
+        /**
+         * Convenience method for accessing the router.
+         * @public
+         * @param {sap.ui.base.Event} oEvent The item select event
+         */
+        onItemSelect: function(oEvent) {
+            var oItem = oEvent.getParameter('item');
+            var sKey = oItem.getKey();
+            // if you click on home, settings or statistics button, call the navTo function
+            if ((sKey === "home" || sKey === "garage")) {
+                // if the device is phone, collaps the navigation side of the app to give more space
+                if (Device.system.phone) {
+                    this.onSideNavButtonPress();
+                }
+                this.getRouter().navTo(sKey);
+            } else {
+                MessageToast.show(sKey);
+            }
+        },
+            
+        onSideNavButtonPress: function() {
+            var oToolPage = this.byId("app");
+            var bSideExpanded = oToolPage.getSideExpanded();
+            //this._setToggleButtonTooltip(bSideExpanded);
+            oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
+        },
+    });
+
+});
---
+++ de.jumajumo.homecontrol.ui/WebContent/resources/de/jumajumo/Component.js
@@ -0,0 +1,233 @@
+jQuery.sap.declare("jumajumo.Component");
+
+sap.ui.core.UIComponent
+		.extend(
+				"jumajumo.Component",
+				{
+
+					metadata :
+					{
+						routing :
+						{
+							config :
+							{
+								viewType : "XML",
+								viewPath : "jumajumo.ha.view",
+								targetControl : "haApp",
+								clearTarget : false,
+								transition : "slide"
+							},
+							routes :
+							[
+							{
+								pattern : "",
+								name : "homeAutomationObjectList",
+								view : "HomeAutomationObjectList",
+								viewPath : "jumajumo.ha.view",
+								viewLevel : 0,
+								preservePageInSplitContainer : true,
+								targetAggregation : "masterPages",
+								subroutes :
+								[
+										{
+											pattern : "homeAutomationObject/garage",
+											name : "homeAutomationObjectGarage",
+											view : "HomeAutomationObjectGarageDetail",
+											viewPath : "jumajumo.ha.view",
+											viewLevel : 2,
+											targetAggregation : "detailPages"
+										},
+										{
+											pattern : "homeAutomationObject/camera",
+											name : "homeAutomationObjectCamera",
+											view : "HomeAutomationObjectCameraDetail",
+											viewPath : "jumajumo.ha.view",
+											viewLevel : 2,
+											targetAggregation : "detailPages"
+										},
+										{
+											pattern : "homeAutomationObject/lightning",
+											name : "homeAutomationObjectLightning",
+											view : "HomeAutomationObjectLightningDetail",
+											viewPath : "jumajumo.ha.view",
+											viewLevel : 2,
+											targetAggregation : "detailPages"
+										},
+										{
+											pattern : "homeAutomationObject/pictures",
+											name : "homeAutomationObjectImageGroupOverview",
+											view : "HomeAutomationObjectImageGroupOverview",
+											viewPath : "jumajumo.ha.view",
+											viewLevel : 2,
+											targetAggregation : "detailPages",
+											subroutes :
+											[
+											{
+												pattern : "homeAutomationObjectImageGroupDetail/{path}",
+												name : "homeAutomationObjectImageGroupDetail",
+												view : "HomeAutomationObjectImageGroupDetail",
+												viewPath : "jumajumo.ha.view",
+												viewLevel : 3,
+												targetAggregation : "detailPages",
+											} ]
+										},
+										{
+											pattern : "hA/picturesGarden",
+											name : "hAImageGroupGardenOverview",
+											view : "HAImageGroupGardenOverview",
+											viewPath : "jumajumo.ha.view",
+											viewLevel : 2,
+											targetAggregation : "detailPages",
+											subroutes :
+											[
+											{
+												pattern : "hAImageGroupGardenDetail/{path}",
+												name : "hAImageGroupGardenDetail",
+												view : "HAImageGroupGardenDetail",
+												viewPath : "jumajumo.ha.view",
+												viewLevel : 3,
+												targetAggregation : "detailPages",
+											} ]
+										},
+										{
+											pattern : "homeControlConfiguration",
+											name : "homeControlConfiguration",
+											view : "HomeControlConfiguration",
+											viewPath : "jumajumo.ha.view.configuration",
+											viewLevel : 2,
+											targetAggregation : "masterPages",
+											subroutes :
+											[
+													{
+														pattern : "homeControlDevicesOverview",
+														name : "homeControlDevicesOverview",
+														view : "HomeControlDevicesOverview",
+														viewPath : "jumajumo.ha.view.configuration",
+														viewLevel : 3,
+														targetAggregation : "detailPages",
+														subroutes :
+														[
+														{
+															pattern : "homeControlDeviceDetail/{path}",
+															name : "homeControlDeviceDetail",
+															view : "HomeControlDeviceDetail",
+															viewPath : "jumajumo.ha.view.configuration",
+															viewLevel : 4,
+															targetAggregation : "detailPages",
+														} ]
+													},
+													{
+														pattern : "homeControlTriggersOverview",
+														name : "homeControlTriggersOverview",
+														view : "HomeControlTriggersOverview",
+														viewPath : "jumajumo.ha.view.configuration",
+														viewLevel : 3,
+														targetAggregation : "detailPages",
+													},
+													{
+														pattern : "homeControlActionsOverview",
+														name : "homeControlActionsOverview",
+														view : "HomeControlActionsOverview",
+														viewPath : "jumajumo.ha.view.configuration",
+														viewLevel : 3,
+														targetAggregation : "detailPages",
+													},
+													{
+														pattern : "homeControlConditionsOverview",
+														name : "homeControlConditionsOverview",
+														view : "HomeControlConditionsOverview",
+														viewPath : "jumajumo.ha.view.configuration",
+														viewLevel : 3,
+														targetAggregation : "detailPages",
+													},
+													{
+														pattern : "homeControlActionChainsOverview",
+														name : "homeControlActionChainsOverview",
+														view : "HomeControlActionChainsOverview",
+														viewPath : "jumajumo.ha.view.configuration",
+														viewLevel : 3,
+														targetAggregation : "detailPages",
+														subroutes :
+														[
+														{
+															pattern : "homeControlActionChainDetail/{path}",
+															name : "homeControlActionChainDetail",
+															view : "HomeControlActionChainDetail",
+															viewPath : "jumajumo.ha.view.configuration",
+															viewLevel : 4,
+															targetAggregation : "detailPages",
+														} ]
+													}, ]
+										}, ]
+
+							} ]
+						}
+					},
+
+					/**
+					 * !!! The steps in here are sequence dependent !!!
+					 */
+					init : function()
+					{
+
+						// 1. some very generic requires
+						jQuery.sap.require("sap.m.routing.RouteMatchedHandler");
+						jQuery.sap.require("jumajumo.ha.MyRouter");
+						jQuery.sap.require("jumajumo.ha.WebSocket");
+
+						// 2. call overridden init (calls createContent)
+						sap.ui.core.UIComponent.prototype.init.apply(this,
+								arguments);
+
+						// 3a. monkey patch the router
+						var router = this.getRouter();
+						router.myNavBack = jumajumo.ha.MyRouter.myNavBack;
+						router.myNavToWithoutHash = jumajumo.ha.MyRouter.myNavToWithoutHash;
+
+						// on none phone devices route to the empty page / keep
+						// the master page
+						// on phone device
+						if (!sap.ui.Device.system.phone)
+						{
+							router.myNavToWithoutHash("jumajumo.ha.view.Empty",
+									"XML", false);
+						}
+
+						// 4. initialize the router
+						this.routeHandler = new sap.m.routing.RouteMatchedHandler(
+								router);
+						router.initialize();
+
+					},
+
+					destroy : function()
+					{
+						if (this.routeHandler)
+						{
+							this.routeHandler.destroy();
+						}
+
+						// call overridden destroy
+						sap.ui.core.UIComponent.prototype.destroy.apply(this,
+								arguments);
+					},
+
+					createContent : function()
+					{
+						// create root view
+						var oView = sap.ui.view(
+						{
+							id : "app",
+							viewName : "jumajumo.ha.view.App",
+							type : "JS",
+							viewData :
+							{
+								component : this
+							}
+						});
+
+						// done
+						return oView;
+					}
+
+				});

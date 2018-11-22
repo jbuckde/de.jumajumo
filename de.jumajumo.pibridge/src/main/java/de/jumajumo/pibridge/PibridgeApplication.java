c184344e7fb6f83cebc41105868858022946b24c
D054910 <j.buck@sap.com>
2018-11-22 12:02:28 +0100

    pibridge initial
    Change-Id: I49a9e80ffdbfaa5e5d33e8058301fe9c6d8bfc8c

---
+++ de.jumajumo.pibridge/src/main/java/de/jumajumo/pibridge/PibridgeApplication.java
@@ -0,0 +1,12 @@
+package de.jumajumo.pibridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PibridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PibridgeApplication.class, args);
+	}
+}
